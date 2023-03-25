dce9b0bfb75c45cce29b0baf601c479d13fabd5a
Somesh5723 <s2somesh237@gmail.com>
2023-03-25 18:17:08 +0530

    second

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,45 @@
 import React from  "react";
 
-class CardItem extends React.Component{
+class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            price :999,
+            title:'Mobile Phone',
+            qty:1,
+            img:''
+        }
+    }
+
+    increaseQuantity =()=> {
+
+    }
     render(){
+        const {price , title , qty} = this.state;
         return(
-            <div className="card-item">
+            <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize:25 }}>Phone</div>
-                    <div style={{ color:'#777'}}>Rs 999</div>
-                    <div style={{ color:'#777'}}>Qty: 1</div>
-                    <div className="card-item-actions">
+                    <div style={{ fontSize:25 }}>{title}</div>
+                    <div style={{ color:'#777'}}>{price}</div>
+                    <div style={{ color:'#777'}}>{qty}</div>
+                    <div className="cart-item-actions">
                         {/* Buttons + - */}
+                        <img 
+                            alt="increase" className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                            onClick={this.increaseQuantity.bind(this)}
+                        />
+                        <img 
+                            alt="decrease" className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+                        />
+                        <img 
+                            alt="delete" className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+                        />
                     </div>
                 </div>
             </div>
@@ -32,4 +59,4 @@ const styles = {
 
 
 
-export default CardItem
+export default CartItem
