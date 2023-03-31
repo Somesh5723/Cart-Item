8af2a73746e34faf0195c6485ef7db85ec653e8d
Somesh5723 <s2somesh237@gmail.com>
2023-03-31 18:45:22 +0530

    Price sum and Cart count functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,113 @@
+import React from 'react';
 import './App.css';
 import CartItem from './CartItem';
 import Cart from './Cart';
+import Navbar from './Navbar';
+class App extends React.Component {
+  
+    constructor(){
+      super();
+      this.state = {
+          products:[
+              {
+                  price :59999,
+                  title:'Mobile Phone',
+                  qty: 1,
+                  img:'https://images.unsplash.com/photo-1616348436168-de43ad0db179?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=481&q=80',
+                  id:1
+              },
+              {
+                  price :69000,
+                  title:'Laptop',
+                  qty: 1,
+                  img:'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
+                  id:2
+              },
+              {
+                  price :174000,
+                  title:'Shoe',
+                  qty: 1,
+                  img:'https://images.unsplash.com/photo-1612724189298-89d36b10b26d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80',
+                  id:3
+              }
+          ]
+      }
+  }
 
-function App() {
-  return (
-    <div className="App">
+  handleIncreaseQuantity = (product) => {
+      const {products} = this.state;
+      const index = products.indexOf(product);
+
+      products[index].qty += 1;
+      this.setState ({
+          products
+      })
+  }
+
+  handleDecreaseQuantity = (product) => {
+      const {products} = this.state;
+      const index = products.indexOf(product);
+
+      if(products[index].qty >= 1){
+          products[index].qty -= 1;
+      }
       
-      <Cart/>
-    </div>
-  );
+      this.setState({
+          products
+      })
+  }
+
+
+  handleDeleteProduct = (id) => {
+      const {products} = this.state
+
+      const items = products.filter((item) => item.id !== id);
+
+      this.setState({
+          products:items
+      })
+  }
+
+  getCartCount  =()=> {
+    const {products} = this.state;
+    let count = 0;
+
+    products.forEach((product)=>{
+      count += product.qty;
+    })
+    return count;
+  }
+
+  getCartTotal =()=> {
+    const {products} = this.state;
+
+    let cartTotal = 0;
+
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price
+     })
+
+    return cartTotal; 
+  }
+ 
+  render() {
+    const {products} = this.state;
+    return (
+      <div className="App">
+        <Navbar  count={this.getCartCount()}/>
+        <Cart 
+          products = {products}
+          onIncreaseQuantity = {this.handleIncreaseQuantity}
+          onDecreaseQuantity = {this.handleDecreaseQuantity}
+          onDeleteProduct = {this.handleDeleteProduct}
+        />
+
+        <div style={{fontSize:20 , padding:10 , color:'green'}}>
+          Total: {this.getCartTotal()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Cart.js
+++ src/Cart.js
@@ -1,75 +1,8 @@
 import React from  "react";
 import CartItem from "./CartItem";
 
-class Cart extends React.Component{
-
-    constructor(){
-        super();
-        this.state = {
-            products:[
-                {
-                    price :19999,
-                    title:'Mobile Phone',
-                    qty: 1,
-                    img:'',
-                    id:1
-                },
-                {
-                    price :69000,
-                    title:'Laptop',
-                    qty: 1,
-                    img:'',
-                    id:2
-                },
-                {
-                    price :774000,
-                    title:'Air Dior Shoe',
-                    qty: 1,
-                    img:'',
-                    id:3
-                }
-            ]
-        }
-    }
-
-    handleIncreaseQuantity = (product) => {
-        const {products} = this.state;
-        const index = products.indexOf(product);
-
-        products[index].qty += 1;
-        this.setState ({
-            products
-        })
-    }
-
-    handleDecreaseQuantity = (product) => {
-        const {products} = this.state;
-        const index = products.indexOf(product);
-
-        if(products[index].qty >= 1){
-            products[index].qty -= 1;
-        }
-        
-        this.setState({
-            products
-        })
-    }
-
-
-    handleDeleteProduct = (id) => {
-        const {products} = this.state
-
-        const items = products.filter((item) => item.id !== id);
-
-        this.setState({
-            products:items
-        })
-    }
-
-
-
-    render(){
-        const  {products} = this.state;
+const Cart =(props)=>{
+       const  {products} = props;
         return(
             <div className="cart">
                 
@@ -78,14 +11,14 @@ class Cart extends React.Component{
                         <CartItem 
                             product = {product} 
                             key={ product.id} 
-                            onIncreaseQuantity = {this.handleIncreaseQuantity}
-                            onDecreaseQuantity = {this.handleDecreaseQuantity}
-                            onDeleteProduct = {this.handleDeleteProduct}
+                            onIncreaseQuantity = {props.onIncreaseQuantity}
+                            onDecreaseQuantity = {props.onDecreaseQuantity}
+                            onDeleteProduct = {props.onDeleteProduct}
                         />
                     )
                 })}
             </div>
         );
-    }
+    
 }
 export default Cart
